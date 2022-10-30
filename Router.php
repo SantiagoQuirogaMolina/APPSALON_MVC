619b84f118e90726af312c868803905a030e840b
Santiago Quiroga <santiago.quiroga@uptc.edu.co>
2022-10-30 00:51:21 -0500

    otro

--- Router.php
+++ Router.php
@@ -7,19 +7,31 @@ class Router
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, $fn) {
+    public function get($url, $fn)
+    {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, $fn) {
+    public function post($url, $fn)
+    {
         $this->postRoutes[$url] = $fn;
     }
 
-    public function comprobarRutas() {
-       // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
-    //$currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-       // $currentUrl = $_SERVER['REDIRECT_URL'] ?? '/';
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+    public function comprobarRutas()
+    {
+        // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
+        //$currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+        // $currentUrl = $_SERVER['REDIRECT_URL'] ?? '/';
+
+        if ($_SERVER['PATH_INFO']) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
+
+
+
+        //$currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
@@ -28,7 +40,7 @@ class Router
             $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
-        if ( $fn ) {
+        if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
@@ -36,7 +48,8 @@ class Router
         }
     }
 
-    public function render($view, $datos = []) {
+    public function render($view, $datos = [])
+    {
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
