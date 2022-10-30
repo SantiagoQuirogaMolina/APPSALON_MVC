315ea8266089f399685203c98e2224ac4cfa9a88
Santiago Quiroga <santiago.quiroga@uptc.edu.co>
2022-10-30 10:34:23 -0500

    mas

--- Router.php
+++ Router.php
@@ -22,7 +22,7 @@ class Router
     
 
 
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
