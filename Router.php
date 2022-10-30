1b86d5f579605a6c0c25bb6e34417c23b49c415e
Santiago Quiroga <santiago.quiroga@uptc.edu.co>
2022-10-30 01:33:02 -0500

    uno mas

--- Router.php
+++ Router.php
@@ -21,22 +21,18 @@ class Router
     {
         // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
         //$currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-         //$currentUrl = $_SERVER['REDIRECT_URL'] ?? '/';
+        //$currentUrl = $_SERVER['REDIRECT_URL'] ?? '/';
 
-        // if ($_SERVER['PATH_INFO']) {
-        //     $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        // } else {
-        //     $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        // }
-        // if (isset($_SERVER['PATH_INFO'])) {
-        //     $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        // } else {
-        //     $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        // }
+    
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
 
 
 
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        // $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
